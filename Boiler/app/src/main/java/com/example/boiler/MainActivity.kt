df628dea8c07530544e4135dbd9b941178f958ff
nvdr <n-v-d-r@yandex.ru>
2022-05-24 19:52:27 +0300

    Initial commit

---
+++ Boiler/app/src/main/java/com/example/boiler/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.boiler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import android.widget.Button
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
